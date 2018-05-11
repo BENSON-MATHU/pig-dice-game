0f979d8f572f56efcad6203c5260546e9e4d67c5
BENSON-MATHU <bensonmuchoki56@gmail.com>
2018-05-11 17:11:17 +0300

    Adding second person button game results and button

--- js/scripts.js
+++ js/scripts.js
@@ -14,18 +14,34 @@ $(document).ready(function(){
   var double ="YOU HAVE A DOUBLE VALUE";
   if(die1===die2){
     $("#message-first-palyer").text(double);
-    $("#roll-dice").click
+    $("#roll-dice").click(function(){
+      $("#message-first-palyer").hide();
+    });
   }
+});
+
+// $("roll-dice").click(fu);
+  $("#sec-palyer-roll-dice").click(function(){
+//back end
+
+    var die3 = Math.ceil(Math.random()*6)+1;
+    var die4 = Math.ceil(Math.random()*6)+1;
+    $("#die3").text(die3);
+    $("#die4").text(die4);
+    var totalScore2= function(){
+      return die3 + die4 ;
+    }
+
+  $("#total2").text(totalScore2());
+  var double2 ="YOU HAVE A DOUBLE VALUE";
+  if(die3===die4){
+    $("#message-second-palyer").text(double2);
+    $("#sec-palyer-roll-dice").click(function(){
+      $("#message-second-palyer").hide();
+    });
+  }
+});
 
 
-  });
-   // if(totalScore===4;){
-   //   function(){
-   //   $("#sec-palyer-roll-dice").show();
-   //     $("#roll-dice").hide();
-   //   }
-   // }else{
-   //  $("#roll-dice").show();
-   // }
-   //  }
+
 });
